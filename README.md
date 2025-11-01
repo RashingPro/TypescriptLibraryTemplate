3611548b1228299196e1b365f282159e805e361c
RashingPro <rashingpro@yandex.ru>
2025-11-01 19:02:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Pay attention
+
+- Due to some changes made in `@eslint/core 0.17.0`, it does not work even with config generated via their `init` command. So I locked `@eslint/core` version to `0.16.0` in `package.json` overrides.
+- Tested with `pnpm@10.20.0`, but technically it should work well with npm and yarn.
---
+++ rollup.config.ts
@@ -0,0 +1,37 @@
+import typescriptPlugin, { RollupTypescriptOptions } from "@rollup/plugin-typescript";
+import terserPlugin from "@rollup/plugin-terser";
+import { RollupOptions } from "rollup";
+import { dts as dtsPlugin } from "rollup-plugin-dts";
+import dtsMinifyPlugin from "rollup-plugin-dts-minify";
+import jsonPlugin from "@rollup/plugin-json";
+
+const outDir = "dist";
+
+export default [
+    {
+        input: "src/index.ts",
+        output: {
+            dir: outDir,
+            format: "es",
+            sourcemap: true
+        },
+        plugins: [
+            typescriptPlugin({
+                noEmitOnError: true
+            }),
+            terserPlugin({
+                keep_classnames: /^.*Error$/,
+                compress: {}
+            }),
+            jsonPlugin()
+        ]
+    },
+    {
+        input: "src/index.ts",
+        output: {
+            dir: outDir,
+            format: "es"
+        },
+        plugins: [dtsPlugin(), dtsMinifyPlugin()]
+    }
+] as (RollupOptions & RollupTypescriptOptions)[];
