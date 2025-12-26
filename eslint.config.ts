b9c9e918412e816634aa8a9919e487430fa4b701
RashingPro <rashingpro@yandex.ru>
2025-12-26 20:39:21 +0300

    chore: move imports style checking to eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,8 @@
 import js from "@eslint/js";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
+import imports from "eslint-plugin-import";
+import unusedImports from "eslint-plugin-unused-imports";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -16,9 +18,13 @@ export default defineConfig([
     { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
     { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
     {
+        plugins: {
+            "unused-imports": unusedImports,
+            import: imports
+        },
         rules: {
             "@typescript-eslint/explicit-member-accessibility": "error",
-            "@typescript-eslint/no-unused-vars": "warn",
+            "@typescript-eslint/no-unused-vars": "off",
             "@typescript-eslint/no-explicit-any": "warn",
             "@typescript-eslint/no-empty-object-type": "off",
             "@typescript-eslint/no-empty-interface": "off",
@@ -33,7 +39,15 @@ export default defineConfig([
                     message:
                         "Getters and setters could create not obvious code execution flow. Please replace with method."
                 }
-            ]
+            ],
+            "import/order": [
+                "error",
+                {
+                    groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"]
+                }
+            ],
+            "import/newline-after-import": "error",
+            "import/no-useless-path-segments": "error"
         }
     },
     {
